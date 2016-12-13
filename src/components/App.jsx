3d34506b9cde2df514f7293103dd4739600d0f83
Mateusz Czeladka <mateusz.szczap@gmail.com>
2016-12-13 22:39:19 +0100

    next

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,24 +3,25 @@ import styles from './../styles/styles.scss';
 import React from 'react';
 import PageList from './PageList';
 import Paper from 'material-ui/Paper';
-
-const flexStyle = {
-    'display': 'flex'
-  };
+import classnames from "classnames";
 
 class App extends React.Component {
-  
+
   render() {
     return (
-      <div className={styles.wrapper}>
-        <div className={[styles.row]}>
+      <div className={classnames(styles.wrapper, styles.redBg)}>
+        <div className={styles.row}>
+
           <div className={styles['col-xs-2']}>
+            <div className={styles.box}>
               <PageList />
+            </div>
           </div>
+          
           <div className={styles['col-xs-10']}>
-              <Paper>
-                test
-              </Paper>
+            <div className={styles.box}>
+              <Paper>s</Paper>
+            </div>
           </div>
         </div>
       </div>
